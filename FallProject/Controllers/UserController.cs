d715653d14fe0b4f9db7cb0f2fca5e1449302ed8
KimNordstrom <Kim.nordstrom@outlook.com>
2022-01-24 15:19:52 +0100

    Added login function to API

---
+++ FallProject/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SERVICES;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpGet("Login")]
+        public IActionResult Login(string userName, string passWord) {
+            var result = UserService.Instance.Login(userName, passWord);
+            if(result == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ SERVICES/UserService.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICES
+{
+    public class UserService
+    {
+        private static UserService _instance;
+        public static UserService Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new UserService();
+                }
+                return _instance;
+            }
+        }
+        private UserService() { }
+
+        public bool Login(string userName, string passWord)
+        {
+            using(var context = new BudgetContext())
+            {
+                var users = context.Users;
+                foreach(var user in users)
+                {
+                    if(user.UserName == userName && user.Password == passWord)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
