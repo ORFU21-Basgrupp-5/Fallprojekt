c211668dc8ace55faf9829e595910772953e0584
Erik <erik.widell@hotmail.com>
2022-01-25 14:37:09 +0100

    Lagt till Services & controllers

---
+++ SERVICES/ExpensesServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICES
+{
+    public class ExpensesServices
+    {
+        private static ExpensesServices _instance;
+        public static ExpensesServices Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ExpensesServices();
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ SERVICES/IncomeServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICES
+{
+    public class IncomeServices
+    {
+        private static IncomeServices _instance;
+        public static IncomeServices Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new IncomeServices();
+                }
+                return _instance;
+            }
+        }
+    }
+}
