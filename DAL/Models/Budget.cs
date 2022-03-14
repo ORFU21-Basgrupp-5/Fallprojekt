c56ce4d72d5ceaaf9fed0c7e700d71fc564a0d48
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-03-14 13:16:47 +0100

    added first drawft for models to add budgets

---
+++ DAL/Models/Budget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public string BudgetName { get; set; }
+        public int TotalSum { get; set; }
+        public int CurrentTotalUsed { get; set; }
+        //Navigation Properties
+        public virtual List<BudgetEntries> BudgetCategories { get; set; }
+
+    }
+}
---
+++ DAL/Models/BudgetEntries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class BudgetEntries
+    {
+        public int Id { get; set; }
+        public CategoryExpense BudgeeCategory{ get; set; }
+        public int CategoryBudgetAmount { get; set; }
+        public int CurrentUsedBudget { get; set; }
+    }
+}
