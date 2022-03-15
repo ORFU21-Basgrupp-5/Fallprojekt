874b20070d84d0f788a24dc8505f4cc433dda679
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-03-15 14:34:30 +0100

    Added Controller and service Barebone functions

---
+++ FallProject/Controllers/BudgetController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using API.DTO;
+using SERVICES;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetController : ControllerBase
+    {
+
+        [Authorize]
+        [HttpPost("Create")]
+        public IActionResult CreateBudget(CreateBudgetDTO Budget)
+        {
+            if (BudgetService.Instance.AddBudget(Budget.Name,Budget.TotalSum,Budget.Month,Budget.Year,Budget.CategoriesAndAmount))
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ SERVICES/BudgetService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace SERVICES
+{
+    public class BudgetService
+    {
+        private static BudgetService _instance;
+        public static BudgetService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new BudgetService();
+                }
+                return _instance;
+            }
+        }
+        public bool AddBudget(string Name, int TotalSum, int Month,int Year, Dictionary<CategoryExpense, int> BudgetCategories)
+        {
+
+
+            return true;
+        }
+    }
+}
